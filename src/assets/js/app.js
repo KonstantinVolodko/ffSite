e8ad322c6fbf0554c6ee3ac539e2012722408681
konstantinVolodko <volodko2000@bk.ru>
2023-03-13 01:03:01 +0300

    kasasmmf

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,28 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
 
 
-    //= components/
+  //= components/
 
 
-    const tl1 = gsap.timeline();
+  const tl1 = gsap.timeline();
 
-    tl1.to('.main-runString__runString ul', { x: '100%', duration: 9, repeat: -1, ease: "none", })
+  tl1.to('.main-runString__runString ul', { x: '100%', duration: 9, repeat: -1, ease: "none", })
 
 
-    let swiper = new Swiper(".mySwiper", {
-        loop: true,
-        autoplay: true,
-    });
+  let swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: true,
+  });
 
-    let swiperDevelop = new Swiper(".mainDevelop", {
-        pagination: {
-          el: ".swiper-pagination",
-        },
-        navigation: {
-            nextEl: ".btnNext",
-            prevEl: ".btnPrev",
-          },
-      });
+  let swiperDevelop = new Swiper(".mainDevelop", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".btnNext",
+      prevEl: ".btnPrev",
+    },
+  });
+
+  let dozeSwiper = new Swiper(".dozeSwiper", {
+    slidesPerView: 2.6,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-paginationDoze",
+      clickable: true,
+    },
+  });
 
 
 
