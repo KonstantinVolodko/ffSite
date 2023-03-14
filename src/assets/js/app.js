06d1f8c375ca8f0b3e84b96b6afa6e533c63aa6f
As <volodko2000@bk.ru>
2023-03-14 17:48:21 +0300

    llllll

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -18,6 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
     pagination: {
       el: ".swiper-pagination",
     },
+    loop: true,
+    autoplay: true,
     navigation: {
       nextEl: ".btnNext",
       prevEl: ".btnPrev",
@@ -25,19 +27,38 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   let dozeSwiper = new Swiper(".dozeSwiper", {
-    slidesPerView: 2.6,
+    slidesPerView: 1.6,
     spaceBetween: 20,
     pagination: {
       el: ".swiper-paginationDoze",
       clickable: true,
     },
+    breakpoints: {
+      700: {
+        slidesPerView: 2.6,
+      },
+    }
   });
 
   const tl2 = gsap.timeline();
 
   tl2.to('.main-sellRunLine ul', { x: '100%', duration: 9, repeat: -1, ease: "none", })
 
+  let activePopap = document.querySelectorAll('#activePopap')
+  let popap = document.querySelector('.main-sell__popap')
+  let cross = document.querySelector('#crossIco')
 
+  console.log(cross)
+
+  activePopap.forEach(e => {
+    e.addEventListener('click', () => {
+      popap.classList.add('transitionActive')
+    })
+  })
+
+  cross.addEventListener('click', () => {
+    popap.classList.remove('transitionActive')
+  })
 
 })
 
